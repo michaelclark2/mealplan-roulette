9950af7ff89cccb7f2a384138af4f5165893799d
Michael Clark <michael.clark1992@gmail.com>
2022-09-02 10:34:46 -0500

    added pinned recipes

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -1,16 +1,34 @@
-import React from "react";
-import { Box, Image, Heading, Columns } from "react-bulma-components";
+import React, { useState } from "react";
+import {
+  Box,
+  Image,
+  Icon,
+  Button,
+  Heading,
+  Columns,
+} from "react-bulma-components";
 import "./RecipeCard.css";
 
-const RecipeCard = ({ recipe }) => {
+const RecipeCard = ({ recipe, togglePinRecipe }) => {
   return (
-    <Box className="RecipeCard">
+    <Box
+      className="RecipeCard"
+      style={{ position: "relative" }}
+      onClick={() => togglePinRecipe(recipe.id)}
+    >
       <Columns
         desktop={{ display: "inline", gap: 0 }}
         mobile={{ display: "flex", gap: 1 }}
         vCentered={true}
       >
         <Columns.Column>
+          {recipe.isPinned ? (
+            <Icon overlay={true} backgroundColor="primary">
+              X
+            </Icon>
+          ) : (
+            ""
+          )}
           <Image src={recipe.image} />
         </Columns.Column>
         <Columns.Column>
--- src/screens/RecipeRouletteScreen.js
+++ src/screens/RecipeRouletteScreen.js
@@ -11,9 +11,41 @@ import { getRandomRecipes } from "../utils/api";
 
 const RecipeRouletteScreen = (props) => {
   const [recipes, setRecipes] = useState([]);
+  const [pinnedRecipes, setPinnedRecipes] = useState([]);
+
+  const pinRecipe = (recipe) => {
+    recipes.splice(recipes.indexOf(recipe), 1);
+
+    setRecipes(recipes);
+    setPinnedRecipes([
+      ...pinnedRecipes.filter((r) => r.id !== recipe.id),
+      recipe,
+    ]);
+  };
+
+  const unPinRecipe = (recipe) => {
+    pinnedRecipes.splice(pinnedRecipes.indexOf(recipe), 1);
+
+    setPinnedRecipes(pinnedRecipes);
+    setRecipes([...recipes.filter((r) => r.id !== recipe.id), recipe]);
+  };
+
+  const togglePinRecipe = (recipeId) => {
+    const recipe =
+      recipes.find((r) => r.id === recipeId) ||
+      pinnedRecipes.find((r) => r.id === recipeId);
+
+    recipe.isPinned = !recipe.isPinned;
+
+    if (recipe.isPinned) {
+      pinRecipe(recipe);
+    } else {
+      unPinRecipe(recipe);
+    }
+  };
 
   const getRecipes = () => {
-    getRandomRecipes(7)
+    getRandomRecipes(7 - pinnedRecipes.length)
       .then((recipes) => setRecipes(recipes))
       .catch((err) => {
         console.error(err);
@@ -35,10 +67,13 @@ const RecipeRouletteScreen = (props) => {
         <Container>
           <Columns justifyContent="center">
             {recipes && recipes.length ? (
-              recipes.map((recipe) => {
+              [...pinnedRecipes, ...recipes].map((recipe) => {
                 return (
                   <Columns.Column key={recipe.id} size="one-quarter">
-                    <RecipeCard recipe={recipe} />
+                    <RecipeCard
+                      recipe={recipe}
+                      togglePinRecipe={togglePinRecipe}
+                    />
                   </Columns.Column>
                 );
               })
