802f62adeb3a7253204076405f31aec7f681b523
Michael Clark <michael.clark1992@gmail.com>
2022-09-27 10:26:27 -0500

    better mealplancard styling

--- src/components/MealPlanCard/MealPlanCard.js
+++ src/components/MealPlanCard/MealPlanCard.js
@@ -1,47 +1,39 @@
 import React from "react";
-import {
-  Box,
-  Button,
-  Columns,
-  Container,
-  Heading,
-} from "react-bulma-components";
+import { Box, Button, Columns, Heading } from "react-bulma-components";
 
 const MealPlanCard = (props) => {
   const mealPlan = { createdAt: Date.now(), recipes: [1, 2, 3, 4] };
   return (
     <Box>
-      <Container className="content">
-        <Columns className="is-mobile">
-          <Columns.Column className="is-narrow">
-            <Heading>
-              {new Date(mealPlan.createdAt).toLocaleDateString("en-us", {
-                weekday: "short",
-                month: "numeric",
-                day: "numeric",
-                year: "numeric",
-              })}
+      <Columns className="is-mobile">
+        <Columns.Column className="is-narrow">
+          <Heading>
+            {new Date(mealPlan.createdAt).toLocaleDateString("en-us", {
+              weekday: "short",
+              month: "numeric",
+              day: "numeric",
+              year: "numeric",
+            })}
+          </Heading>
+        </Columns.Column>
+        <Columns.Column className="is-flex" justifyContent="end">
+          <Button color="danger">x</Button>
+        </Columns.Column>
+      </Columns>
+      <ul className="is-flex-desktop is-align-items-center">
+        {mealPlan.recipes.map((x) => (
+          <li className="is-block-desktop m-0">
+            <Heading
+              renderAs="a"
+              href="https://google.com"
+              target="_blank"
+              size={5}
+            >
+              Chicken Fajitas
             </Heading>
-          </Columns.Column>
-          <Columns.Column className="is-flex" justifyContent="end">
-            <Button color="danger">x</Button>
-          </Columns.Column>
-        </Columns>
-        <ul>
-          {mealPlan.recipes.map((x) => (
-            <li>
-              <Heading
-                renderAs="a"
-                href="https://google.com"
-                target="_blank"
-                size={5}
-              >
-                Chicken Fajitas
-              </Heading>
-            </li>
-          ))}
-        </ul>
-      </Container>
+          </li>
+        ))}
+      </ul>
     </Box>
   );
 };
