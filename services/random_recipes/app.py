7e81b6ddaaa715e360be2926b33a1f31dcc25dfd
Michael Clark <michael.clark1992@gmail.com>
2022-10-25 09:42:41 -0500

    sends request to spoonacular, returns results array

--- services/random_recipes/app.py
+++ services/random_recipes/app.py
@@ -1,5 +1,18 @@
 import os
+import requests
 
 def handler(event, context):
+    # TODO: use context to read identity from cognito user pools
+
     api_key = os.environ["SPOONACULAR_APIKEY"]
-    return api_key
+    params = {
+        "apiKey" : api_key,
+        "addRecipeInformation": True,
+        "sort": "random",
+        "type": "main course",
+        "number": event["number"],
+        "diet": ",".join(event["diets"]),
+        "intolerances": ",".join(event["intolerances"])
+    }
+    request = requests.get("https://api.spoonacular.com/recipes/complexSearch", params=params)
+    return request.json()['results']
