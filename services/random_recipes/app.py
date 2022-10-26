930c491274972d9824a95ae44e76a8a118aa9450
Michael Clark <michael.clark1992@gmail.com>
2022-10-25 22:42:57 -0500

    reads queryStringParams from lambda event from api gateway

--- services/random_recipes/app.py
+++ services/random_recipes/app.py
@@ -4,16 +4,18 @@ import requests
 def handler(event, context):
     api_key = os.environ["SPOONACULAR_APIKEY"]
 
-    assert event["number"] < 8
+    params = event['queryStringParameters']
+
+    assert int(params["number"]) < 8
 
     params = {
         "apiKey" : api_key,
         "addRecipeInformation": True,
         "sort": "random",
         "type": "main course",
-        "number": event["number"],
-        "diet": ",".join(event["diets"]),
-        "intolerances": ",".join(event["intolerances"])
+        "number": params["number"],
+        "diet": ",".join(params.get("diets", [])),
+        "intolerances": ",".join(params.get("intolerances", []))
     }
     request = requests.get("https://api.spoonacular.com/recipes/complexSearch", params=params)
     return request.json()['results']
